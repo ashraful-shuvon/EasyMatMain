f6e7d6c5f985307858619d883a4f3395b54ad8ae
Ashraful Shuvon <ashraful.shuvon@gmail.com>
2026-08-12 15:50:45 +0600

    Organized the project a little.

--- Assets/ScriptsAR/EggMarkerUI.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using Mapbox.Utils;
-
-public class EggMarkerUI : MonoBehaviour
-{
-    public EggType eggType;
-    public Vector2d gpsPosition;
-    public bool isScannable;
-}
